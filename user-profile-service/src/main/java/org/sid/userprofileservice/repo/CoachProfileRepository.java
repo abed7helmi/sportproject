bee00c0095988050f711dcd5c9ef215510df2643
el-hadj <elhadj.balde@etu.u-pec.fr>
2022-12-08 19:22:27 +0100

    deleting coach information from user profile micro service

--- user-profile-service/src/main/java/org/sid/userprofileservice/repo/CoachProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sid.userprofileservice.repo;
-
-import org.sid.userprofileservice.entities.CoachProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoachProfileRepository extends JpaRepository<CoachProfile, Integer> {
-}
