7fc466929d2a5af9a7b74401a109183045b9e8a0
el-hadj <elhadj.balde@etu.u-pec.fr>
2022-12-08 19:48:20 +0100

    adding restTemplate for user service

--- user-profile-service/src/main/java/org/sid/userprofileservice/UserProfileServiceApplication.java
+++ user-profile-service/src/main/java/org/sid/userprofileservice/UserProfileServiceApplication.java
@@ -2,12 +2,19 @@ package org.sid.userprofileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserProfileServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserProfileServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
